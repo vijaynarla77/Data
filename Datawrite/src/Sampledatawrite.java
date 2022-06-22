b55b37d3460072afe0400d9a4c395ea61a9a8450
vijaynarla77 <vijay77narla@gmail.com>
2022-06-22 12:25:46 +0530

    First commit

---
+++ Datawrite/src/Sampledatawrite.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Sampledatawrite {
+	
+	File f1;
+	FileInputStream fs;
+	XSSFWorkbook wb;
+	XSSFSheet xsff;
+	public void sample(String Sheetname,int row,int column,String cellvalue) throws Exception
+	{
+		f1=new File("C:\\Users\\msa\\Desktop\\kcsm2\\Datawrite\\Dataexcel\\Book1.xlsx");
+		
+		fs = new FileInputStream(f1);
+		wb = new XSSFWorkbook(fs);
+		
+		xsff = wb.getSheet(Sheetname);
+		xsff.getRow(row).createCell(column).setCellValue(cellvalue);
+		
+		FileOutputStream fos = new FileOutputStream(f1);		
+	}
+
+}
